1f92abbf791bd38b6d7fa84db81d45ecc9aedbac
alex_ <alex_adinuta@yahoo.com>
2021-02-02 16:22:10 +0000

    Added Assignment 2 files

---
+++ 3 Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+alert("Welcome to my website!")
+alert("Here you will find information about animals")
+
+function My_First_Function() {
+var String = "Kiss me, I'm Irish!";
+var result = String.fontcolor("green");
+document.getElementById("Irish").innerHTML =
+result;
+}
